5e530b215f0a1e5dc3d8ab2060cc46d93d02d5a5
Bernhard Kölbl <besentv@gmail.com>
2024-11-28 20:38:12 +0100

    Restructure repo.

--- config/ccache_config.sh
+++ config/ccache_config.sh
@@ -1,1 +1,5 @@
+#
+# Links system compilers to ccache, automatically enabling it for every compilation.
+#
+
 sudo mkdir -p /usr/lib/ccache && ls /usr/bin/{,*-}{cc,c++,gcc,g++}{,-[0-9]*} | sed -re s:/bin:/lib/ccache: | xargs -n1 ln -sf ../../bin/ccache
